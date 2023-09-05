fd778649f7c3744614e917c97dacd932e247159a
Faisal Tamim <61521038+faisaltamim@users.noreply.github.com>
2023-09-05 09:55:06 +0600

    initialize

---
+++ js/custom.js
@@ -0,0 +1,51 @@
+
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+  $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+
+
+
+    // CUSTOM LINK
+  $('.custom-link').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height() + 10;
+
+
+    scrollToDiv(elWrapped,header_height);
+
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+        scrollTop: totalScroll
+      }, 300);
+    }
+  });
+
+
+
+
+
+  // my custom js
+  // var childElement = $(".navbar");
+  // var parentClass = childElement.parent().attr("class");
+  // console.log(parentClass);
+  // if ($('#navbar').clossets('is-sticky')) {
+  //   consol.log("ache");
+  // } else {
+  //   consol.log('nai');
+  // }
+
+})(window.jQuery);
+
+
